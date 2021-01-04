263829511fe5e67032d63ca9a96ab21f6793aa54
Jurgen Emanuels <Jurgen.Emanuels@CyberIAM.com>
2021-01-04 15:30:58 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  courses = COURSES;
+
+  startDate = new Date(2000, 0, 1);
+
+  title = COURSES[0].description;
+
+  price = 9.99;
+
+  rate =  0.67;
+
+  course = COURSES[0];
+
+
+  // coreCourse = COURSES[0];
+  // rxjsCourse = COURSES[1];
+  // ngrxCourse = COURSES[2];
+
+  onCourseSelected(course: Course){
+    console.log('App component - click event bubbled ...', course);
+  }
+
+}
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { UrlHandlingStrategy } from '@angular/router';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+  
+  @Input()
+  course: Course;
+
+  @Input()
+  cardIndex: number;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get isImageVisible(){
+    return this.course && this.course.iconUrl;
+  }
+
+  onCourseViewed(){
+    console.log('card component - button clicked ...');
+    this.courseSelected.emit(this.course);
+  }
+
+  cardClasses(){
+
+    if(this.course.category == 'BEGINNER'){
+      return 'beginner';
+    }
+  }
+
+  cardStyles(){
+    return {'background-image': 'url(' + this.course.iconUrl + ')'};
+  }
+
+}
