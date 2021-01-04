dab26c347e42b9d50248bd9b9b57be1ac59c3fa5
Jurgen Emanuels <Jurgen.Emanuels@CyberIAM.com>
2021-01-04 17:11:36 +0200

    The AfterViewInit Lifecycle Hook - How deep can we query with View Child? We can only see the current template and we can't go deeper than that.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,33 @@
-import { ElementRef, ViewChild } from '@angular/core';
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { Course } from './model/course';
+import { AfterViewInit, ElementRef, ViewChild } from "@angular/core";
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { Course } from "./model/course";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-
+export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
- @ViewChild('cardRef1', {read: ElementRef})
+  @ViewChild("cardRef1", { read: ElementRef })
   card1: ElementRef;
 
-  @ViewChild('cardRef2')
+  @ViewChild("cardRef2")
   card2: CourseCardComponent;
 
-  @ViewChild('container')
+  @ViewChild("container")
   containerDiv: ElementRef;
 
-  onCourseSelected(course: Course){
-    console.log(this.card1);
-    console.log(this.card2);
-
-    console.log('containerDiv', this.containerDiv);
-    
+  constructor() {
+    console.log("containerDiv", this.card1);
   }
 
+  onCourseSelected(course: Course) {}
+
+  ngAfterViewInit() {
+    console.log("containerDiv", this.card1);
+  }
 }
